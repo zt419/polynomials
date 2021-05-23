35a68b9518e4d6e8372d4f9fede7fb8903002bd0
Tang Zhiqi <zt419@imperial.ac.uk>
2021-05-23 14:08:07 +0100

    .

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -13,7 +13,7 @@ class Polynomial:
         terms = []
 
         if coefs[0] > 0:
-        #Can say if coefs[0] only
+        # Can say if coefs[0] only
             terms.append(str(coefs[0]))
 
         if self.degree() > 0 and coefs[1] > 0:
@@ -23,6 +23,35 @@ class Polynomial:
                  for d, c in enumerate(coefs[2:], start=2) if c]
 
         return " + " .join(reversed(terms)) or "0"
+
+    def __eq__(self, other):
+
+        return self.coefficients == other.coefficients
+    
+    def __add__(self, other):
+        if isinstance(other, Polynomial):
+        # Check if other is a polynomial
+            common = min(self.degree(), other.degree()) + 1
+            coefs = tuple(a + b for a, b in zip(self.coefficients, other.coefficients))
+            coefs += self.coefficients[common:] + other.coefficients[common:]
+
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            return Polynomial((self.coefficients[0] + other,) + self.coefficients[1:])
+
+        else:
+            return NotImplemented
+            
+    def __radd__(self, other):
+        return self + other
+
+
+
+
+
+
+
 
 
             
