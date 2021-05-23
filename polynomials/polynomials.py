5f677c16d0844577b8b17633746b6297cab50535
Tang Zhiqi <zt419@imperial.ac.uk>
2021-05-23 13:20:58 +0100

    .

---
+++ polynomials/polynomials.py
@@ -0,0 +1,30 @@
+class Polynomial:
+    def __init__(self, coefs):
+
+        self.coefficients = coefs
+
+    def degree(self):
+
+        return len(self.coefficients) - 1 
+
+    def __str__(self):
+
+        coefs = self.coefficients
+        terms = []
+
+        if coefs[0] > 0:
+        #Can say if coefs[0] only
+            terms.append(str(coefs[0]))
+
+        if self.degree() > 0 and coefs[1] > 0:
+            terms.append(f"{'' if coefs[1] == 1 else coefs[1]}x")
+        
+        terms += [f"{'' if c == 1 else c}x^{d}" 
+                 for d, c in enumerate(coefs[2:], start=2) if c]
+
+        return " + " .join(reversed(terms)) or "0"
+
+
+            
+
+
